8f51300b4235e478f87dc8b113ac4b802769526b
niesssiobhan <niesssiobhan@live.com>
2019-01-06 11:10:27 -0800

    resubmittion

--- app.js
+++ app.js
@@ -35,4 +35,30 @@ function fetchPeopleWithPromises() {
     .catch();
 }
 
+let fetchPeopleWithAsync = async () => {
+
+  try{
+
+  let peopleSet = await superagent.get('https://swapi.co/api/people/');
+  let people = (peopleSet.body && peopleSet.body.results) || [];
+  let peopleRequests = people.map((person) => {
+    return superagent.get(person.url);
+  });
+  let swapiNames = await Promise.all(peopleRequests)
+  .then(people => {
+    let names = [];
+    for(let data of people) {
+      names.push(data.body.name);
+    }
+    return names;
+  });
+  return swapiNames;
+}
+
+catch(e) {console.error(e);}
+};
+
+fetchPeopleWithAsync()
+.then(names => console.log('Async', names));
+
 fetchPeopleWithPromises();
